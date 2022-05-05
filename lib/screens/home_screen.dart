7874b9619fc5bf25fba660092ebdb5e0badc0342
AgustLuj <alujan@openpass.com.ar>
2022-05-05 11:20:26 -0300

    modify the list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,8 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-import 'package:my_app/screens/screens.dart';
-
-class Account {
-  final String name;
-  final IconData icon;
-  const Account({required this.name, required this.icon});
-}
+import 'package:my_app/models/menu_option.dart';
+import 'package:my_app/router/app_routes.dart';
 
 class HomeScreen extends StatelessWidget {
-  final options = const [
-    Account(name: "Agustin", icon: Icons.account_circle),
-    Account(name: "Fabian", icon: Icons.account_circle),
-    Account(name: "Pablo", icon: Icons.account_circle),
-    Account(name: "Martin", icon: Icons.account_circle),
-    Account(name: "Marcos", icon: Icons.account_circle),
-  ];
-
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
@@ -28,26 +13,26 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: ListView.separated(
-          itemCount: options.length,
-          itemBuilder: (context, i) =>itemBuilder(context, i,options[i]) ,
-          separatorBuilder: (_, __) => const Divider(),
-        )
-      ),
+          child: ListView.separated(
+        itemCount: AppRoutes.getCount(),
+        itemBuilder: (context, i) =>
+            itemBuilder(context, i, AppRoutes.menuOption[i]),
+        separatorBuilder: (_, __) => const Divider(),
+      )),
     );
   }
 }
 
-itemBuilder(BuildContext context,int i,Account  options){
+itemBuilder(BuildContext context, int i, MenuOption options) {
   return ListTile(
     leading: Icon(options.icon),
     title: Text(options.name),
     trailing: const Icon(Icons.chevron_right_sharp),
     onTap: () {
-      final route = MaterialPageRoute(
-        builder:(context) => CardScreen(name: options.name),
-      );
-      Navigator.push(context,route);
+      // final route = MaterialPageRoute(
+      //   builder: (context) => options.screen,
+      // );
+      Navigator.pushNamed(context, options.route);
     },
   );
 }
